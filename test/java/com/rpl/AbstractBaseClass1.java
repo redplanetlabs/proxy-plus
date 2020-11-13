695b918b575c51f067c2b2185b2871b0d532054c
Bryan Duxbury <bryan@redplanetlabs.com>
2020-11-12 22:36:25 -0500

    better support for over-specified abstract class hierarchies

---
+++ test/java/com/rpl/AbstractBaseClass1.java
@@ -0,0 +1,11 @@
+package com.rpl;
+
+public abstract class AbstractBaseClass1 implements InterfaceD {
+
+  public String bar(Integer x, Integer y, long z) {
+    return null;
+  };
+
+  protected abstract String foo (Integer x, String y);
+
+}
