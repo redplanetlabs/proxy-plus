664d76708ed5248710a60fd80db4e17a73d6e887
Bryan Duxbury <bryan@redplanetlabs.com>
2021-08-25 14:22:50 -0400

    bump version and changelog

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rpl/proxy-plus "0.0.4"
+(defproject com.rpl/proxy-plus "0.0.5"
   :description "A faster and more usable replacement for Clojure's proxy."
   :java-source-paths ["test/java"]
   :test-paths ["test/clj"]
