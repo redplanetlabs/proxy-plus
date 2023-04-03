63c8b58cc367998627a10bd74f4b410178a0625b
Philip Dumaresq <phdumaresq@protonmail.com>
2023-04-03 17:56:02 -0400

    Upgraded ASM version to 5.1

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :test-paths ["test/clj"]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [com.rpl/specter "1.1.3"]
-                 [org.ow2.asm/asm "4.2"]
-                 [org.ow2.asm/asm-commons "4.2"]
-                 [org.ow2.asm/asm-util "4.2"]
-                 [org.ow2.asm/asm-analysis "4.2"]]
+                 [org.ow2.asm/asm "5.1"]
+                 [org.ow2.asm/asm-commons "5.1"]
+                 [org.ow2.asm/asm-util "5.1"]
+                 [org.ow2.asm/asm-analysis "5.1"]]
 
   :profiles {:bench {:dependencies [[criterium "0.4.5"]]}
              }
